e2d0c6b8adb4e277b37191bf452af22c296012ab
bhimashankar <bhimashankarhugar2004@gmail.com>
2026-02-16 12:47:16 +0530

    work

--- smpl2.py
+++ smpl2.py
@@ -1,2 +1,9 @@
-print("hello")
-print("hi bhima")
+import csv
+with open("student.csv", "r") as file:
+    reader = csv.DictReader(file)
+    
+    print("Students who Passed:")
+    
+    for row in reader:
+        if row["Status"] == "Pass":
+            print(row["Name"])
---
+++ task3.py
@@ -0,0 +1,8 @@
+filename = input("Enter the filename: ")
+try:
+    with open(filename, "r") as file:
+        content = file.read()
+        print("\nFile Content:")
+        print(content)
+except FileNotFoundError:
+    print("Oops! That file doesn't exist yet.")
